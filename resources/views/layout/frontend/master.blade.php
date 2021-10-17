58b1201a14ee1309a1c512b640e4f2404a384135
daffaayovanka <myovanka.19044@mhs.unesa.ac.idgit config --global user.email myovanka.19044@mhs.unesa.ac.id>
2021-10-17 19:33:14 +0700

    second commit

---
+++ resources/views/layout/frontend/master.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" lang="en">
+  <head>
+    
+    <title>Yov' Profile</title>
+    <meta charset="UTF-8">
+    
+    <meta property="og:title" content="Serenity - A Portfolio Template">
+    <meta property="og:image" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Serenity - A Portfolio Template">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-compatible" content="IE=Edge">
+    <meta name="robots" content="index, follow">
+    
+    <meta name="description" content="This is a portfolio template, coded in HTML 5 and CSS 3!">
+    <meta name="keywords" content="Portfolio Template HTML5 CSS3 Javascript Custom">
+    <meta name="msapplication-TileColor" content="#cdcec0">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
+    <link rel="stylesheet" href="css/all.css">
+    
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <script src="js/jquery-2.1.4.min.js"></script>
+  </head>
+  <body>
+    
+    <header>
+      <div class="overlay-nav hidden">
+        <ul class="mobile">
+          <li><a href="/">CV</a></li>
+        </ul>
+        <ul class="mobile">
+          <li><a href="#" class="work">Gallery</a></li>
+        </ul>
+        <ul class="mobile">
+          <li><a href="#" class="contact-nav">CONTACT ME</a></li>
+        </ul>
+        <div class="close"></div>
+      </div>
+      <div class="container">
+        <div id="logo">Yov' Profile</div>
+        <nav><a id="toggle" href="#"><i class="fa fa-bars fa-2x"></i></a>
+          <ul class="in">
+            <li><a href="/">CV</a></li>
+          </ul>
+          <ul class="in">
+            <li><a href="#" class="work">Gallery</a></li>
+          </ul>
+          <ul class="in">
+            <li><a href="#" class="contact-nav">CONTACT ME</a></li>
+          </ul>
+        </nav>
+        <h1 id="jumbotron">Welcome to My Profile</h1>
+        <h2 id="supporting-header">M. Aaqilah Daffaa Yovanka</h2><a href="mailto:tatasari1972@gmail.com" class="contact-button">Contact Me</a>
+      </div>
+    </header>
+
+@yield('content')
+
+    <footer>
+      <div class="container">
+        <p class="foot">Daffaa Yovanka</p>
+      </div>
+    </footer>
+    <script src="/js/main.js"></script>
+  </body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,51 @@
+@extends('layout.frontend.master')
+@section('content')
+
+<section>
+    <div class="container">
+      <h1 class="port">About Me</h1>
+      <div class="self-pic"></div>
+      
+      <p class="about-para">M. Aaqilah Daffaa Yovanka - Profile .</p>
+      <p class="about-para">S1 Pendidikan Teknologi Informasi 2019 - 19050974044.</p>
+      <p class="about-para">Head of Public Relation Himpunan Mahasiswa Jurusan Teknik Informatika.</p>
+    </div>
+  </section>
+  <section>
+    
+    <h2 class="port">My Gallery</h2>
+    <div class="container">
+      <div class="flex">
+        
+        <figure id="one">
+          <figcaption id="img1" class="overlay">HIMTI UNESA</figcaption><a href="http://externallinkgoeshere.co.uk" target="_blank"><img src="/image/himti.jpeg" alt=""></a>
+        </figure>
+        <figure id="two">
+          <figcaption id="img2" class="overlay">Holiday</figcaption><a href="http://externallinkgoeshere.co.uk" target="_blank"><img src="/image/holiday.jpeg" alt=""></a>
+        </figure>
+        <figure id="three">
+          <figcaption id="img3" class="overlay">Travelling</figcaption><a href="http://externallinkgoeshere.co.uk" target="_blank"><img src="/image/travel.jpeg" alt=""></a>
+        </figure>
+        <figure id="four">
+          <figcaption id="img4" class="overlay">Job Camellia</figcaption><a href="http://externallinkgoeshere.co.uk" target="_blank"><img src="/image/job.jpeg" alt=""></a>
+        </figure>
+        <figure id="five">
+          <figcaption id="img5" class="overlay">Wedding Organizer</figcaption><a href="http://externallinkgoeshere.co.uk" target="_blank"><img src="/image/wo.jpeg" alt=""></a>
+        </figure>
+        <figure id="six">
+          <figcaption id="img6" class="overlay">Photo</figcaption><a href="http://externallinkgoeshere.co.uk" target="_blank"><img src="/image/poto.jpeg" alt=""></a>
+        </figure>
+      </div>
+    </div>
+  </section>
+  <section class="grey">
+    <div class="container">
+      
+      <h2 class="contact">Contact Me</h2>
+      <p class="contact">Instagram @daffaayovanka </p>
+      <p class="contact">Whatsapp +62889-9561-1795</p><a href="mailto:tatasari1972@gmail.com" class="contact-button">Email me</a>
+    
+      </div>
+    </div>
+  </section>
+@stop
